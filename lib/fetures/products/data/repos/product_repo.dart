adda63f65cef814ac052b873e95ab370d57a0229
Fyala <www.ahmedfyala621@gmail.com>
2023-12-01 02:20:42 +0200

    create product repo,faliure class,add either

---
+++ lib/fetures/products/data/repos/product_repo.dart
@@ -0,0 +1,8 @@
+import 'package:either_dart/either.dart';
+
+import '../../../../core/errors/faliure.dart';
+import '../models/product.dart';
+
+abstract class ProductRepo {
+  Future<Either<Faliure,List<Product>>> fetchProducts();
+}
